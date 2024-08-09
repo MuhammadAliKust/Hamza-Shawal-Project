6d3f600321ecd5a975eb8aae24d73df507008645
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-09 16:09:59 +0500

    Single Selection Done

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ListViewDemo extends StatelessWidget {
+class ListViewDemo extends StatefulWidget {
   const ListViewDemo({super.key});
+
+  @override
+  State<ListViewDemo> createState() => _ListViewDemoState();
+}
+
+class _ListViewDemoState extends State<ListViewDemo> {
+  int selectedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +16,21 @@ class ListViewDemo extends StatelessWidget {
       appBar: AppBar(
         title: Text("List View Demo"),
       ),
-      body: ListTile(
-        leading: Icon(Icons.notifications),
-        title: Text("Notification Title"),
-        subtitle: Text("Notification Sub Title"),
-        tileColor: Colors.blue,
-        trailing: Icon(Icons.arrow_forward_ios),
-      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, i) {
+            return ListTile(
+              onTap: () {
+                selectedIndex = i;
+                setState(() {});
+              },
+              leading: Icon(Icons.notifications),
+              title: Text("Index $i Selected Index $selectedIndex"),
+              subtitle: Text("Notification Subtitle"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              tileColor: selectedIndex == i ? Colors.blue : Colors.white,
+            );
+          }),
     );
   }
 }
